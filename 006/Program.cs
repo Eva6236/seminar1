65bb4bdf53c5c7aa7c2ecae2ffa3094bcb5cbfe9
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:13:05 +0300

    006

---
+++ 006/Program.cs
@@ -0,0 +1,31 @@
+﻿//Написать программу вычисления значения функции y = sin(a).
+/*double x,y;
+x=Math.PI;
+y=Math.Sin(x);
+System.Console.WriteLine($"sin({x})={y}");
+
+double r=1.0/3;
+System.Console.WriteLine(r);
+*/
+
+// тип возвращаемого значения Имя_функции(аргументы функции)
+/*double Sin(double x); //методы
+{
+    return Math.Sin(x);
+}
+
+double Sum(double a, double b)
+{
+    return a+b;
+}
+
+
+void Pause()//метод
+{
+    System.Console.WriteLine("Press any key");
+}
+
+double y=Sin(3.14);
+System.Console.WriteLine(y);
+Pause();
+*/
