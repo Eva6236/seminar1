4616a4908f0220be10c258e018ba6d42d5245e69
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:14:07 +0300

    008

---
+++ 008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести на экран числа от -N до N
+int N, N1; 
+System.Console.WriteLine("Введите отрицательное число: ");
+string? s=Console.ReadLine();
+N = Convert.ToInt32(s); 
+System.Console.WriteLine("Введите положительное число: ");
+string? d=Console.ReadLine();
+N1 = Convert.ToInt32(d); 
+while(N<=N1)
+{
+    {
+        System.Console.WriteLine( N );
+    }
+    N++;
+}
