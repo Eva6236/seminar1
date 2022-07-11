df8ec41885898a6b4c2813caa87df7de8d5f5c91
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:14:58 +0300

    010

---
+++ 010/Program.cs
@@ -0,0 +1,8 @@
+﻿//Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+System.Console.WriteLine("Введите число: ");
+int n, y;
+string? s=Console.ReadLine();
+n = Convert.ToInt32(s); 
+y = n % 10;
+Console.WriteLine($"Последняя цифра числа - { y }");
+ 
