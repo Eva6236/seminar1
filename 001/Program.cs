c9d660875d9d94e1ed29ab2c6d2b03f03aafe479
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:07:01 +0300

    001

---
+++ 001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Console.WriteLine(11%3);
+// С клавиатуры вводится целое число. Вывести квадрат числа 
+
+double a;
+double b;
+string? s = Console.ReadLine();
+a =Convert.ToDouble(s);
+b= a*a;
+System.Console.WriteLine("{0}^2={1}", a,b); //строка форматирования
+System.Console.WriteLine($"{a}^2={b}"); // строка интерполяции
+
+/*
+string? s;
+s = Console.ReadLine();
+
+System.Console.WriteLine(s);
+*/
