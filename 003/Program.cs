00bdb5f7c13de86e4cdb089967356b777f76a9a6
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:11:28 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+int a,b; 
+
+System.Console.WriteLine("Введите первое число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.WriteLine("Введите второе число: ");
+string? e =Console.ReadLine();
+b = Convert.ToInt32(e);
+if (a > b)
+{
+    System.Console.WriteLine("первое - максимальное число");
+}
+else 
+{
+    System.Console.WriteLine("второе - максимальное число");
+}
+    
