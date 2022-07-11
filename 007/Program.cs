edccc37da81732709160ccfbb7a9d5d3510dd5e3
Eva Klets <Evaklets@mail.ru>
2022-07-11 21:13:38 +0300

    007

---
+++ 007/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным.
+int a;
+//int b;
+int d = 2;
+System.Console.WriteLine("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+//b = a % d; 
+//System.Console.WriteLine($"b = {b}");
+if (a % d != 0) 
+{
+    System.Console.WriteLine("Нечетное число ");
+} else 
+{ 
+    System.Console.WriteLine("Четное число ");
+}
