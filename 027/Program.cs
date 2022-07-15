1ca6e467b5f3cbe2aa75d3c47637aa639368ee0d
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:13:53 +0300

    027

---
+++ 027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+/*int a, b, c;
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.Write("Введите степень: ");
+string? e = Console.ReadLine();
+b = Convert.ToInt32(e);
+c = Math.Pow(a, b);
+System.Console.WriteLine(c);
+*/
+int a, b, i, c;
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.Write("Введите степень: ");
+string? e = Console.ReadLine();
+b = Convert.ToInt32(e);
+ c = 1;
+for(i=1;i<=b;i++)
+{
+    c *= a;
+}
+System.Console.WriteLine(c);
+
