51d6b74c474a9531b2a32c633a7c50951a3cb891
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:14:25 +0300

    031

---
+++ 031/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+int N, i, multi;
+Console.Write("Введите кол-во чисел: ");
+string? s = Console.ReadLine();
+N = Convert.ToInt32(s);
+ 
+Console.WriteLine("Кубы чисел, оканчивающиеся на четную цифру: ");
+for (i = 1; i <= N; i++) {
+    multi = i*i*i;
+    if((multi % 2) == 0) {
+        System.Console.WriteLine(multi);
+    }
+}
