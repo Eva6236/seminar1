f1c0f897b0c66314d095c342aa89d8243e10cc2f
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:11:55 +0300

    010

---
+++ 021/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат.
+double a;
+System.Console.WriteLine("Введите номер четверти: ");
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+if (a == 1){
+System.Console.WriteLine("Диапазон x > 0, y > 0 ");
+}
+if (a == 2){
+System.Console.WriteLine("Диапазон x < 0, y > 0 ");
+}
+if (a == 3){
+System.Console.WriteLine("Диапазон x < 0, y < 0 ");
+}
+if (a == 4){
+System.Console.WriteLine("Диапазон x > 0, y < 0 ");
+}
+else{
+    System.Console.WriteLine("Не верно введен номер четверти");
+}
