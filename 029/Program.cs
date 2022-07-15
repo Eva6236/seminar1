c4f2c984f8afb363c17e6c291dee9ba740b1b0d3
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:14:07 +0300

    029

---
+++ 029/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе
+int a, b;
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b = 0;
+while (a > 0){
+    b = b + a % 10;
+    a = a /10 ;
+ }
+ Console.WriteLine(b);
