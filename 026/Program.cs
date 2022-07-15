49437df480e7084ed565016b6600ceb6883300b5
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:13:45 +0300

    026

---
+++ 026/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А. 
+int i, A; 
+int sum = 0; 
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+A = Convert.ToInt32(s);
+for (i=1; i <=A; i++) {
+sum = sum+i;
+}
+System.Console.WriteLine(sum);
+
