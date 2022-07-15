1a8c0f74b00e6f4aa7f63dd4021fee7f4cc7af42
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:13:38 +0300

    025

---
+++ 025/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран кубы чисел от 1 до N.
+int N, i;
+Console.Write("Введите кол-во чисел: ");
+string? s = Console.ReadLine();
+N = Convert.ToInt32(s);
+ 
+Console.WriteLine("Кубы чисел: ");
+ for(i = 1; i <= N; i++)
+Console.WriteLine(i*i*i);
