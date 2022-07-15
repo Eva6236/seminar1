a56d5a566f8b6c58100dba9d73a13194b821cfa6
Eva Klets <Evaklets@mail.ru>
2022-07-15 12:14:17 +0300

    030

---
+++ 030/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int i, A; 
+int multi = 1; 
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+A = Convert.ToInt32(s);
+for (i=1; i <=A; i++) {
+multi = multi  * i;
+}
+System.Console.WriteLine(multi);
+
