fb5aba9a1ffa98a37a7e6b1dd10a713877ad4bfc
Eva Klets <Evaklets@mail.ru>
2022-07-20 18:27:39 +0300

    035

---
+++ 035/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу замены элементов массива на противоположные
+Console.WriteLine("Введите размер массива: ");
+int n=int.Parse(Console.ReadLine());
+int [] a=new int[n];
+Random random=new Random();
+    for (int i = 0; i < n; i++) 
+      {
+    a[i] = random.Next(-10, 10);
+}
+    for(int i = 0; i < a.Length; i++)
+System.Console.Write($"{a[i], 4}");
+ 
+for (int i = 0;i < n; i++) {
+      a[i] =  -1 * a[i];
+}
+for (int i = 0;i < n; i++) {
+System.Console.Write($"\n{a[i], 4} ");
+}
+      
+
