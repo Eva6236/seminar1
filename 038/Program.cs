bdd65f7a2d746a868a21740dd2bce212e9327073
Eva Klets <Evaklets@mail.ru>
2022-07-20 18:28:34 +0300

    038

---
+++ 038/Program.cs
@@ -0,0 +1,18 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int n = 123;
+int [] a=new int[n];
+Random random=new Random();
+    for (int i = 0; i < n; i++)
+        {
+            a[i] = random.Next(0, 124);
+        }
+    for(int i = 0; i < n; i++)
+System.Console.Write($"{a[i], 4}");
+int count_1 = 0;
+for(int i = 0; i < n; i++)
+    if (i > 10 && i < 100)
+    {
+        count_1 +=1; 
+                    
+    }
+    System.Console.Write($"\nКоличество элементов {count_1, 4}");
