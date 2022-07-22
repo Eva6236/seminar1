de7170ce4033963335a146e2f10668ae1fbf971b
Eva Klets <Evaklets@mail.ru>
2022-07-22 12:57:16 +0300

    043

---
+++ 043/Program.cs
@@ -0,0 +1,18 @@
+﻿//Определить сколько чисел больше 0 введено с клавиатуры.
+Console.WriteLine("Введите размер массива: ");
+int n=int.Parse(Console.ReadLine());
+int [] a=new int[n];
+Random random=new Random();
+    for (int i = 0; i < n; i++) {
+            a[i] = random.Next(-20, 21);
+            System.Console.Write($"{a[i], 4}");
+        }
+int moreZero = 0;
+for (int i = 0; i < a.Length; i++ )
+    if (zero(a[i])) {
+        moreZero++;
+    }
+System.Console.WriteLine($"\nКоличество чисел больше 0: {moreZero}" );
+bool zero(int val) { 
+    return val > 0 ? true : false;
+}
